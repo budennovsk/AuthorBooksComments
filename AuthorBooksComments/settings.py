ac00fbd25557fdd5a86bf921c08a08dab772e595
Zin <svetloewill@mail.ru>
2023-06-28 14:45:42 +0300

    secret, delete,add

--- AuthorBooksComments/settings.py
+++ AuthorBooksComments/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from config import SECRET_KEY, NAME, PORT, HOST, PASSWORD, USER
+from config import SECRET_KEY, NAME, PORT, HOST, PASSWORD, USER, DEBUG, ALLOWED_HOSTS
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ALLOWED_HOSTS]
 
 # Application definition
 
